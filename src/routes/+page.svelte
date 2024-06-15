191e4797c449dd541356fdc69018eae033662869
dominicf2001 <dominicf2001@gmail.com>
2024-06-15 08:43:24 -0400

    Setup tailwind and daisyui. Some version fixing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,1 @@
-<script lang="ts">
-    import "../app.css";
-
-    const title = "App";
-    const name: string = "dominic";
-</script>
-
-<h1 class="font-bold">Hello world!</h1>
-
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-</style>
+<button class="btn">Hello daisyUI</button>
