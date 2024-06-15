f7d92778542aafa1db5f5a335510dd4ec79ea313
dominicf2001 <dominicf2001@gmail.com>
2024-06-15 18:27:36 -0400

    setup initial grid and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "../app.css";
+
+    const { children } = $props();
+</script>
+
+<main class="h-screen flex justify-center items-center">
+    {@render children()}
+</main>
+
+<style lang="postcss">
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,34 @@
-<button class="btn bg-primary">Hello daisyUI</button>
+<script lang="ts">
+</script>
+
+<div class="grid grid-cols-2 gap-10">
+    <div class="card w-96 bg-primary shadow-xl">
+        <figure><img alt="Chart" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+    </div>
+
+    <div class="card w-96 bg-primary shadow-xl">
+        <figure><img alt="Chart" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+    </div>
+    <div class="card w-96 bg-primary shadow-xl">
+        <figure><img alt="Chart" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+    </div>
+    <div class="card w-96 bg-primary shadow-xl">
+        <figure><img alt="Chart" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+    </div>
+</div>
